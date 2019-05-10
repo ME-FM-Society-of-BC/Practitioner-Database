d882459eb6d13d0409cfcc792cec09b1201b2db6
Robert <git@robert-thomas.ca>
2019-05-10 16:51:26 -0700

    New actions for moderator processing

--- client/src/store/userActions.js
+++ client/src/store/userActions.js
@@ -1,9 +1,34 @@
 /**
  * Actions associated with users and user roles
  */
+import axios from 'axios';
 
 /******* Synchronous actions *********/
 export const STORE_LOGGED_IN_USER = 'STORE_LOGGED_IN_USER';
 export const STORE_USER_ROLES = 'STORE_USER_ROLES';
 export const STORE_ALL_USERS = 'STORE_ALL_USERS';
+export const STORE_NEW_USER = 'STORE_NEW_USER';
+export const STORE_MODERATORS = 'STORE_MODERATORS';
+export const SAVE_MODERATOR = 'SAVE_MODERATOR';
 export const LOGOUT = 'LOGOUT';
+
+/******* Asynchronous actions ********/
+
+/**
+ * Sends a new Moderator to the server then dispatches to the store
+ * @param {*} moderator 
+ */
+export const saveModerator = (moderator => {
+    return dispatch => {
+        axios.post('/moderators', moderator)
+            .then((response) => {
+                moderator.id = response.data;
+                dispatch({ type: SAVE_MODERATOR, moderator })
+            })
+            .catch(error => {
+                console.log(error);
+                alert(error);
+            }
+        );        
+    }
+})
--- client/src/store/userReducer.js
+++ client/src/store/userReducer.js
@@ -5,53 +5,93 @@ import * as actions from './userActions';
 
 const initialState = {
     allUsers: [],
+    moderators: [],
     roles: {},
     loggedInUser: null
 }
 
 const userReducer = (state = initialState, action) => {
     switch (action.type){
-        case actions.STORE_USER_ROLES :
-
-            // Convert the array of UserRole object to a map of role id to type
-            const roleMap = action.roles.reduce( (map, role) => {
-                map[role.id] = role;
-                return map;
-            }, {});
-            // Store the map rather than the array
-            return {
-                ...state,
-                roles: roleMap 
-            }
-            
-        case actions.STORE_LOGGED_IN_USER :
-            // If the user is a Moderator, add an "isModerator" property]
-            const user = action.user;
-            user.isModerator = user.role === 'MODERATOR';
-            user.isAdministrator = user.role === 'ADMINISTRATOR';
-            return {
-                ...state,
-                loggedInUser: user
-            }
-
-        case actions.STORE_ALL_USERS :
-            const allUsers = action.users.reduce((map, user) => {
-                map[user.id] = user;
-                return map
-            }, {});
-            return {
-                ...state,
-                allUsers: allUsers
-            }
-
-        case actions.LOGOUT :
-            return {
-                ...state,
-                loggedInUser: null
-            }
+        case actions.STORE_USER_ROLES : return storeUserRoles(action.roles, state);
+        case actions.STORE_LOGGED_IN_USER : return storeLoggedInUser(action.user, state);
+        case actions.STORE_ALL_USERS : return storeAllUsers(action.users, state);
+        case actions.LOGOUT : return logout(state);
+        case actions.STORE_NEW_USER : return storeNewUser(action.user, state);
+        case actions.STORE_MODERATORS: return storeModerators(action.moderators, state);
+        case actions.SAVE_MODERATOR: return saveModerator(action.moderator, state);
 
         default: return state;
     }
 }
+const storeUserRoles = (roles, state) => {
+    // Convert the array of UserRole object to a map of role id to type
+    const roleMap = roles.reduce( (map, role) => {
+        map[role.id] = role;
+        return map;
+    }, {});
+    // Store the map rather than the array
+    return {
+        ...state,
+        roles: roleMap 
+    }
+}
+
+const storeLoggedInUser = (user, state) => {
+    // If the user is a Moderator, add an "isModerator" property]
+    user.isModerator = user.role === 'MODERATOR';
+    user.isAdministrator = user.role === 'ADMINISTRATOR';
+    return {
+        ...state,
+        loggedInUser: user
+    }   
+}
+
+const storeAllUsers = (users, state) => {
+    const allUsers = users.reduce((map, user) => {
+        map[user.id] = user;
+        return map
+    }, {});
+    return {
+        ...state,
+        allUsers
+    }
+}
+
+const logout = (state) => {
+    return {
+        ...state,
+        loggedInUser: null
+    }
+}
+
+const storeModerators = (moderators, state) => {
+    return {
+        ...state,
+        moderators
+    }
+}
+
+const storeNewUser = (user, state) => {
+    // This is for the User account of a Moderator created by the Administrator
+    user.isModerator = true;
+    const allUsers = {...state.allUsers};
+    allUsers[user.id] = user;
+    return {
+        ...state,
+        allUsers
+    }
+}
+
+const saveModerator = (moderator, state) => {
+    // This is for the User account of a Moderator created by the Administrator
+    const moderators = [...state.moderators];
+    moderators.push(moderator);
+    return {
+        ...state,
+        moderators
+    }
+}
+
+
 
 export default userReducer;
