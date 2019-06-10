b053995f4093e1e31d06b5aaf049b534d8ce4ae8
Robert <git@robert-thomas.ca>
2019-06-10 06:33:55 -0700

    Adde home page content

--- client/src/containers/MyActivity.js
+++ client/src/containers/MyActivity.js
@@ -2,13 +2,42 @@
  * Implements the MyActivity View
  */
 import React, { Component } from 'react';
+import Instructions from '../components/Instructions';
+import { connect } from 'react-redux';
 
 class MyActivity extends Component {
 
     render() {
-        return <h1>MyActivity</h1>
+        const active = this.props.loggedInUser && !this.props.loggedInUser.isModerator && !this.props.loggedInUser.isAdministrator;
+        if (active){
+            return (
+                <>
+                <Instructions width='80%'>
+                    Welcome to My Activity page, your personal page where you can view and edit the submissions and comments you have made. 
+                    As a registered user you can add and rate a new practitioner, rate an already existing practitioner, or add comments.
+                </Instructions>
+                <div>Activitiy List Under Construction</div>
+                </>
+            )
+        }
+ 
+        else {
+            return (
+                <div>
+                    Instructions for Moderator or Admnistrator
+                </div>
+            )
+        }
     }
 
 }
 
-export default MyActivity;
+const mapStateToProps = state => {
+    return {
+        loggedInUser: state.userReducer.loggedInUser,
+    }
+}
+
+
+// connect(mapStateToProps, mapDispatchToProps)(PractitionerEval)
+export default connect(mapStateToProps)(MyActivity);
