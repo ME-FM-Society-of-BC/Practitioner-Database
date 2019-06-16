5b6d595abb24e6c3799a44094f145d943f8e77e3
Robert <git@robert-thomas.ca>
2019-06-16 14:33:28 -0700

    Removed unused commment edit code

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -14,46 +14,35 @@
  *  
  */
 import React from 'react';
-import ExpandingText from './ExpandingText';
 
 const comment = (props) => {
     const classes = props.level === 1 ? 'comment' : 'comment level2';
     const dateOptions = {year:'numeric', month:'long', day:'numeric', hour:'numeric', minute:'numeric' };
-    if (props.mode === 'edit'){
-        // TODO allow editing?
-        return (
-            <>
-            <ExpandingText class={classes} value={props.value}/>
-            </>
-        )
-    }
-    else { // 'view'
-        const text = props.status === 'BLOCKED' ? 'Comment has been removed by moderator' : props.text
-        return (
-            <div className={classes}>
-                <div className='comment-header'>
-                    <div className='comment-name'>{props.username}</div>
-                    <div className='comment-date'>{props.date.toLocaleDateString("en-US", dateOptions)}</div>
-                    { props.level === 1 && props.enabled ?
-                        <input type='button' className='comment-button comment-reply' 
-                            value='Reply' onClick={props.onClickReply}></input>
-                        : ''
-                    }
-                    { props.status === 'FLAGGED' ?
-                        <span className="comment-button comment-flag fas fa-flag"/>
-                        : ''
-                    }
-                    { props.status === 'PENDING' && props.enabled ?
-                        <input type='button' className='comment-button comment-flag' 
-                            value='Flag' onClick={props.onClickFlag}></input>
-                        : ''
-                    }
-                </div>
-                <div>
-                    {text}
-                </div>
+    const text = props.status === 'BLOCKED' ? 'Comment has been removed by moderator' : props.text
+    return (
+        <div className={classes}>
+            <div className='comment-header'>
+                <div className='comment-name'>{props.username}</div>
+                <div className='comment-date'>{(new Date(props.date)).toLocaleDateString("en-US", dateOptions)}</div>
+                { props.level === 1 && props.enabled ?
+                    <input type='button' className='comment-button comment-reply' 
+                        value='Reply' onClick={props.onClickReply}></input>
+                    : ''
+                }
+                { props.status === 'FLAGGED' ?
+                    <span className="comment-button comment-flag fas fa-flag"/>
+                    : ''
+                }
+                { props.status === 'PENDING' && props.enabled ?
+                    <input type='button' className='comment-button comment-flag' 
+                        value='Flag' onClick={props.onClickFlag}></input>
+                    : ''
+                }
             </div>
-        )
-    }
+            <div>
+                {text}
+            </div>
+        </div>
+    )
 }
 export default comment;
