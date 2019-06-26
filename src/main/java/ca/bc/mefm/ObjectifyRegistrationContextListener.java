61e1c7f498d5a4ed63835d2152e923b8d58fab13
Robert <git@robert-thomas.ca>
2019-06-26 13:51:41 -0700

    New resource

--- src/main/java/ca/bc/mefm/ObjectifyRegistrationContextListener.java
+++ src/main/java/ca/bc/mefm/ObjectifyRegistrationContextListener.java
@@ -7,6 +7,7 @@ import com.googlecode.objectify.ObjectifyService;
 
 import ca.bc.mefm.data.City;
 import ca.bc.mefm.data.Comment;
+import ca.bc.mefm.data.DatastoreVersion;
 import ca.bc.mefm.data.Moderator;
 import ca.bc.mefm.data.PasswordReset;
 import ca.bc.mefm.data.Practitioner;
@@ -42,6 +43,7 @@ public class ObjectifyRegistrationContextListener implements ServletContextListe
     	ObjectifyService.register(City.class);
     	ObjectifyService.register(Moderator.class);
     	ObjectifyService.register(PasswordReset.class);
+    	ObjectifyService.register(DatastoreVersion.class);
     }
 
     @Override
