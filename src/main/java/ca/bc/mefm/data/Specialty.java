de9c62a4159bfa703ffc3842b3c60b97329719d2
rtthomas <git@robert-thomas.ca>
2019-07-25 22:17:43 -0700

    Add specialty grouping

--- src/main/java/ca/bc/mefm/data/Specialty.java
+++ src/main/java/ca/bc/mefm/data/Specialty.java
@@ -6,13 +6,22 @@ import com.googlecode.objectify.annotation.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Defines a Practitioner specialty 
+ * @author Robert
+ */
 @Entity
 @Data
 @AllArgsConstructor
 public class Specialty {
+	
+	public static final String CONVENTIONAL = "Conventional Medicine";
+	public static final String ALTERNATIVE = "Complementary & Alternative";
+	
 	@Id
 	private Long	id;
 	private String	text;
+	private String 	group;
 	
 	public Specialty() {}
 }
