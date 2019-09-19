08cf9e20f292e931d8435764c4ed9b1482412106
rtthomas <git@robert-thomas.ca>
2019-09-18 18:19:20 -0700

    Cleanup

--- client/src/containers/CopyUrl.js
+++ client/src/containers/CopyUrl.js
@@ -17,21 +17,11 @@ class CopyUrl extends Component {
         this.state = {
             url: url
         }
-//        this.show = this.show.bind(this);
-//        this.hide = this.hide.bind(this);
         this.done = this.done.bind(this);
     }
- 
-    // show() {
-    //     this.setState({show: true})
-    // }
-    // hide() {
-    //     this.setState({show: false})
-    // }
     done(){
         this.setState({copied: true})
     }
-// <a className='copy-url' href='#' style={{color: '#ffffff'}} onClick={this.show}>Share</a>
     render() {
         return (
             <Modal 
@@ -63,39 +53,6 @@ class CopyUrl extends Component {
                 </Modal.Footer>
             </Modal>
         )
-        // return (
-        //     <>
-            
-        //     <Modal 
-        //         show={this.state.show} 
-        //         aria-labelledby="contained-modal-title-vcenter" 
-        //         centered='true' 
-        //         onHide={this.hide}>
-        //         <Modal.Header closeButton>
-        //             <Modal.Title id="contained-modal-title-vcenter">
-        //             Want to share this with a friend?
-        //             </Modal.Title>
-        //         </Modal.Header>
-        //         <Modal.Body>
-        //             <div className='centered' style={{width: '15em'}}>
-        //                 <p>Send them this URL:</p>
-        //                 <input type='text' value={this.state.url} className='info-field info-field-reg' readOnly/>
-        //                 <div style={{marginTop: '10px'}}>
-        //                     <CopyToClipboard onCopy={this.onCopy} text={this.state.url}>
-        //                         <button onClick={this.done}>Copy it to the clipboard</button>
-        //                     </CopyToClipboard>
-        //                     {this.state.copied ?
-        //                         <span class="fas fa-check"></span> : ''
-        //                     }
-        //                 </div>
-        //             </div>
-        //         </Modal.Body>
-        //         <Modal.Footer>
-        //             <Button variant="secondary" onClick={this.hide}>Close</Button>
-        //         </Modal.Footer>
-        //     </Modal>
-        //     </>
-        // )
     }
 }
 
