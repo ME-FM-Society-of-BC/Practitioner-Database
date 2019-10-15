9d5137bed48ba411d6804aaf2ca840dd724741dc
rtthomas <git@robert-thomas.ca>
2019-10-14 19:38:36 -0700

    Log bounced emails

--- src/main/java/ca/bc/mefm/mail/BounceHandlerServlet.java
+++ src/main/java/ca/bc/mefm/mail/BounceHandlerServlet.java
@@ -4,7 +4,9 @@ import com.google.appengine.api.mail.BounceNotification;
 import com.google.appengine.api.mail.BounceNotificationParser;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.logging.Logger;
+import java.util.logging.Level;
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,26 +14,21 @@ import javax.servlet.http.HttpServletResponse;
 
 public class BounceHandlerServlet extends HttpServlet {
 
-	private static final Logger log = Logger.getLogger(BounceHandlerServlet.class.getName());
+    private static final Logger log = Logger.getLogger(BounceHandlerServlet.class.getName());
 
-	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		try {
-			BounceNotification bounce = BounceNotificationParser.parse(req);
-			//      log.warning("Bounced email notification.");
-			// The following data is available in a BounceNotification object
-			String from = bounce.getOriginal().getFrom();
-			String to = bounce.getOriginal().getTo() ;
-			// bounce.getOriginal().getSubject() 
-			// bounce.getOriginal().getText() 
-			// bounce.getNotification().getFrom() 
-			// bounce.getNotification().getTo() 
-			// bounce.getNotification().getSubject() 
-			// bounce.getNotification().getText() 
-			// ...
-		} 
-		catch (MessagingException e) {
-			log.severe("BounceHandlerServlet " + e);
-		}
-	}
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            BounceNotification bounce = BounceNotificationParser.parse(req);
+            String from = bounce.getOriginal().getFrom();
+            String to = bounce.getOriginal().getTo() ;
+            String subject = bounce.getOriginal().getSubject();
+            
+            String s = MessageFormat.format("Bounced message from {0} to {1} subject '{2}'", from, to, subject);
+            log.log(Level.WARNING, s);
+        }
+        catch (MessagingException e) {
+            log.severe("BounceHandlerServlet " + e);
+        }
+    }
 }
