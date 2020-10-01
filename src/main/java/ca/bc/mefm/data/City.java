1599404e6a500e374f4d0dde2e501f5ee0d8ee66
rtthomas <git@robert-thomas.ca>
2020-10-01 16:52:46 -0700

    Added index for provinceId

--- src/main/java/ca/bc/mefm/data/City.java
+++ src/main/java/ca/bc/mefm/data/City.java
@@ -2,6 +2,7 @@ package ca.bc.mefm.data;
 
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
 
 import lombok.Data;
 
@@ -10,6 +11,7 @@ import lombok.Data;
 public class City {
 	@Id
 	private Long	id;
+	@Index 
 	private Long	provinceId;
 	private String	name;
 	
